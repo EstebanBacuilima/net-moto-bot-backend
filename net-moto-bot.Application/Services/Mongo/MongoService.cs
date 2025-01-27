029e8b7bf5336630ec2b4e8ec476fbc57ea07218
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2025-01-26 23:04:30 -0500

    feat: add new changes

---
+++ net-moto-bot.Application/Services/Mongo/MongoService.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using net_moto_bot.Application.Interfaces.Mongo;
+using net_moto_bot.Domain.Enums.Custom;
+using net_moto_bot.Domain.Exceptions.BadRequest;
+using net_moto_bot.Domain.Interfaces.Mongo;
+using System.Text.Json;
+
+namespace net_moto_bot.Application.Services.Mongo;
+
+public class MongoService(
+    IMongoRepository _repository) : IMongoService
+{
+    private readonly JsonSerializerOptions _serializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true,
+    };
+
+    public async Task<Dictionary<string, object?>> AddOrUpdate(string _id, Dictionary<string, object?> document)
+    {
+        string collectionName = "chats";
+
+        string json = JsonSerializer.Serialize(document.ToDictionary(
+            kvp => char.ToLowerInvariant(kvp.Key[0]) + kvp.Key[1..],
+            kvp => kvp.Value
+        ), _serializerOptions);
+
+        BsonDocument bsonDocument = BsonDocument.Parse(json);
+
+        // Check if exist document
+        bool existDocument = await _repository.ExistsByCodeAndIdAsync(
+            collectionName: collectionName, string.Empty
+        );
+        if (existDocument)
+        {
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", _id);
+            await _repository.UpdateAsync(collectionName, filter, bsonDocument);
+        }
+        else
+        {
+            await _repository.SaveAsync(collectionName, bsonDocument);
+        }
+        return bsonDocument.ToDictionary();
+    }
+
+}
