8d7410014b5eda75b85dee7e4cc2de4e00706230
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2025-02-14 21:11:44 -0500

    feat: add new changes

---
+++ net-moto-bot.API/Controllers/V1/Test/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace net_moto_bot.API.Controllers.V1.Test;
+
+[Route("api/v1/test")]
+[ApiController]
+//[Authorize]
+public class TestController() : CommonController
+{
+    [HttpGet, Route("run")]
+    public IActionResult GetRun()
+    {
+        return Ok("Service Running...");
+    }
+}
