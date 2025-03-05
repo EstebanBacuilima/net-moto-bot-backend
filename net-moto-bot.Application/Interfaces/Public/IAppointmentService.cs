06c7a85149d4f3d03553a012c5e4994d36ee1a9f
EstebanBacuilima <davidbacuilima13@gmail.com>
2025-03-04 20:52:48 -0500

    feat: appointment service

--- net-moto-bot.Application/Interfaces/Public/IAppointmentService.cs
+++ net-moto-bot.Application/Interfaces/Public/IAppointmentService.cs
@@ -1,6 +1,9 @@
-﻿namespace net_moto_bot.Application.Interfaces.Public;
+﻿using net_moto_bot.Domain.Entities;
+
+namespace net_moto_bot.Application.Interfaces.Public;
 
 public interface IAppointmentService
 {
-
+    public Task<Appointment> CreateAsync(Appointment appointment);
+    public Task<List<Appointment>> GetAllByDateAndIdCardAsync(DateTime date, string idCard);
 }
---
+++ net-moto-bot.Application/Services/Public/AppointmentService.cs
@@ -0,0 +1,20 @@
+﻿using net_moto_bot.Application.Interfaces.Public;
+using net_moto_bot.Domain.Entities;
+using net_moto_bot.Domain.Interfaces.Public;
+
+namespace net_moto_bot.Application.Services.Public;
+
+public class AppointmentService(
+    IAppointmentRepository _repository
+) : IAppointmentService
+{
+    public Task<Appointment> CreateAsync(Appointment appointment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Appointment>> GetAllByDateAndIdCardAsync(DateTime date, string idCard)
+    {
+        return _repository.FindAllAsync(date,  customerIdCard: idCard);
+    }
+}
