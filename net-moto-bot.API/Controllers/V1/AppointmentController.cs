5770938f0092c1cb562234b93254b42bc2dbb680
EstebanBacuilima <davidbacuilima13@gmail.com>
2025-03-09 11:00:47 -0500

    feat: new web services

--- net-moto-bot.API/Controllers/V1/AppointmentController.cs
+++ net-moto-bot.API/Controllers/V1/AppointmentController.cs
@@ -22,4 +22,15 @@ public class AppointmentController(
     {
         return Ok(ResponseHandler.Ok(await _service.CreateAsync(_mapper.Map<Appointment>(request))));
     }
+
+    [HttpGet, Route("list")]
+    public async Task<IActionResult> GetAllAsync(
+        [FromQuery(Name = "date")] DateTime date,
+        [FromQuery(Name = "name")] string name = "",
+        [FromQuery(Name = "customer-id-card")] string customerIdCard = "",
+        [FromQuery(Name = "employee-id-card")] string employeeIdCard = ""
+    )
+    {
+        return Ok(ResponseHandler.Ok(await _service.GetAllByDateAndIdCardAsync(date, customerIdCard, employeeIdCard, name)));
+    }
 }
